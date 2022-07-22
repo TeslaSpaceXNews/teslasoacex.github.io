b852209b520b0700345c2fecb365ac7d1f119fd7
SpaceXGivex <109495838+TeslaSpaceXNews@users.noreply.github.com>
2022-07-22 11:52:52 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function copied(id_element) {
-    var aux = document.createElement("input");
-    aux.setAttribute("value", document.getElementById(id_element).innerHTML);
-    document.body.appendChild(aux);
-    aux.select();
-    document.execCommand("copy");
-    document.body.removeChild(aux);
-  }
-  
